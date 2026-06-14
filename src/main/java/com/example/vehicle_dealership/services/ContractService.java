f0c754bc5f8410dd4161f3c3c2593b78e53e486d
jaxpro-code <jax.bradley.pro@gmail.com>
2026-06-14 17:11:23 -0400

    contract service formatting

---
+++ src/main/java/com/example/vehicle_dealership/services/ContractService.java
@@ -0,0 +1,19 @@
+package com.example.vehicle_dealership.services;
+
+import com.example.vehicle_dealership.repositories.ContractRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContractService {
+    @Autowired
+    private ContractRepository contractRepository;
+
+    //create
+    //read
+    //update
+    //delete
+    //by id
+    //by contract type
+    //by vehicle vin
+}
